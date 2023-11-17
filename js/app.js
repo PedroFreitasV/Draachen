efcbe077f0604adcc73fce95e901884cb4e8897d
pedro <pedrofreitashv@gmail.com>
2023-11-17 13:35:43 -0300

    Atualização

---
+++ js/app.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({reset: true})
+
+sr.reveal('#dragon', {duration: 1000})
+
+sr.reveal('#row', {duration: 1000})
+
+sr.reveal('#vil', {duration: 1000})
+
+sr.reveal('#history', {duration: 1000})
